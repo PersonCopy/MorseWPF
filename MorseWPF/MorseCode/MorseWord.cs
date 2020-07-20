8cabf3ed0ffdd18a8e8fa34cfd77093f14fbeeed
Benjamin Zoronjic <bzoronjic@online.gibz.ch>
2020-07-20 14:59:00 +0200

    MorseWord class created

--- MorseWPF/MorseCode/MorseWord.cs
+++ MorseWPF/MorseCode/MorseWord.cs
@@ -9,17 +9,45 @@ namespace MorseWPF.MorseCode
 {
     public class MorseWord
     {
+        // static array of random words
         public static string[] RandWords = null;
 
+        // dictionary with string (the character) and
+        // a bool? (if character was spelled correctly,
+        // incorrectly or not yet spelled)
+        public Dictionary<string, bool?> SelectedWord;
+
         public MorseWord()
+        {
+            // checks if list has been created
+            if (RandWords == null)
+            {
+                InitRandWords();
+            }
+
+            //initialize
+            SelectedWord = new Dictionary<string, bool?>();
+
+            // selects a random word from the array
+            Random random = new Random();
+            string word = RandWords[random.Next(RandWords.Length + 1)];
+
+            foreach (char c in word)
+            {
+                SelectedWord.Add(c.ToString(), null);
+            }
+        }
+
+        /// <summary>
+        /// Inserts the random words from
+        /// the text file into the RandWords
+        /// string array
+        /// </summary>
+        public static void InitRandWords()
         {
             using (StreamReader r = new StreamReader(@"..\..\MorseCode\MorseData\wordsList.txt"))
             {
                 RandWords = r.ReadToEnd().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            }
-            foreach (string s in RandWords)
-            {
-                Console.WriteLine(s);
             }
         }
     }
