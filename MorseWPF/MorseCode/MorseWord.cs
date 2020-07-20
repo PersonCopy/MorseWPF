05c6362e622d21cdf5d836b72e675abfdbe0eed9
Benjamin Zoronjic <bzoronjic@online.gibz.ch>
2020-07-20 14:14:13 +0200

    code cleanup

---
+++ MorseWPF/MorseCode/MorseWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseWPF.MorseCode
+{
+    public class MorseWord
+    {
+        public static string[] RandWords = null;
+
+        public MorseWord()
+        {
+            using (StreamReader r = new StreamReader(@"..\..\MorseCode\MorseData\wordsList.txt"))
+            {
+                RandWords = r.ReadToEnd().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (string s in RandWords)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
